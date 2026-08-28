19ae4331de48c22e90dac7dc5494fa8f4226df43
Mukunda Parajuli <mukundaparajuli13@gmail.com>
2026-08-29 00:41:21 +0545

    feat: add initial Prisma contract for Batch and BatchUrl models with enums and storage configuration

--- packages/db/src/prisma/db.ts
+++ packages/db/src/prisma/db.ts
@@ -1,8 +1,6 @@
 import 'dotenv/config';
 import postgres from '@prisma/orm-postgres/runtime';
-// @ts-ignore
 import type { Contract } from './contract.d';
-// @ts-ignore
 import contractJson from './contract.json' with { type: 'json' };
 
 export const db = postgres<Contract>({
